b923ec662f3a67e49c7b5ea7a6a88e4baf3e9753
BZsombor7 <csapatmzs@gmail.com>
2026-06-01 20:58:26 +0200

    navbar upgrade cssel egyutt

--- BenedekFinal/frontend/src/components/NavBar.jsx
+++ BenedekFinal/frontend/src/components/NavBar.jsx
@@ -1,16 +1,18 @@
 import { NavLink } from "react-router-dom";
-import { useAuth } from "../context/loginContext"
-import classes from "../components/NavBar.module.css"
+import { useAuth } from "../context/loginContext";
+import styles from "./NavBar.module.css";
 
 const NavBar = () => {
-    const {isLogged, logout} = useAuth()
-    return(
-        <nav>
-            <NavLink to="/" className={({isActive}) =>isActive ? classes.active : classes.link}>Termékek listázása</NavLink>
-            {isLogged && <NavLink to='form' className={({isActive}) =>isActive ? classes.active : classes.link}>Termékek hozzáadása</NavLink>}
-            {!isLogged ? <NavLink to='login' className={({isActive}) =>isActive ? classes.active : classes.link}>Bejelentkezés</NavLink> : <a onClick={logout} className={classes.isActive}>Kijelentkezés</a>}
-        </nav>
-    )
-}
+  const { isLogged, logout } = useAuth();
 
-export default NavBar
+  return (
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={({ isActive }) => isActive ? styles.active : styles.link}>Termékek listázása</NavLink>
+
+      {isLogged && (<NavLink to="/form" className={({ isActive }) => isActive ? styles.active : styles.link}>Termék hozzáadása</NavLink>)}
+
+      {!isLogged ? (<NavLink to="/login" className={({ isActive }) => isActive ? styles.active : styles.link}>Bejelentkezés</NavLink>) : (<button className={styles.logoutBtn} onClick={logout}>Kijelentkezés</button>)}</nav>
+  );
+};
+
+export default NavBar;
